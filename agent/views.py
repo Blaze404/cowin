df35b46ee958cc7cb213e6c89f3327f79f47fa0c
Blaze404 <mus.qazi999@gmail.com>
2021-05-26 16:11:37 +0530

    agents add

--- agent/views.py
+++ agent/views.py
@@ -11,7 +11,7 @@ def index(request):
 
 def add(request):
     if request.method == 'GET':
-        all_agents = F.get_unique_agents()
+        all_agents = list(F.get_unique_agents())
         return render(request, 'transactions.html', {'all_agents': all_agents})
     if request.method == 'POST':
         print(request.POST)
